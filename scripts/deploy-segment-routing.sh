150df3f93adff4e33907776c6e8d5e689a2accba
user <user@example.com>
2025-09-16 18:36:56 +0000

    Update deploy-segment-routing script to use correct file paths for segment routing topology

--- scripts/deploy-segment-routing.sh
+++ scripts/deploy-segment-routing.sh
@@ -1,8 +1,8 @@
 xr-compose \
-  --input-file ~/sandbox/docker-compose.xr.yml \
-  --output-file ~/sandbox/docker-compose.yml \
+  --input-file ~/sandbox/topologies/segment-routing/docker-compose.xr.yml \
+  --output-file ~/sandbox/topologies/segment-routing/docker-compose.yml \
   --image xrd-control-plane:latest-24.4
 
-sed -i.bak 's/linux:xr-120/linux:eth0/g' ~/sandbox/docker-compose.yml
+sed -i.bak 's/linux:xr-120/linux:eth0/g' ~/sandbox/topologies/segment-routing/docker-compose.yml
 
-docker compose --file ~/sandbox/docker-compose.yml up --detach
+docker compose --file ~/sandbox/topologies/segment-routing/docker-compose.yml up --detach
