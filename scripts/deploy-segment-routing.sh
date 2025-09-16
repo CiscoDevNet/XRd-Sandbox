75bb2f4093cce9a140f0c9b34a8eca256b21dde8
user <user@example.com>
2025-09-16 18:40:19 +0000

    Update deploy-segment-routing script to use correct file paths and remove backup file

--- scripts/deploy-segment-routing.sh
+++ scripts/deploy-segment-routing.sh
@@ -1,8 +1,8 @@
 xr-compose \
-  --input-file ~/sandbox/topologies/segment-routing/docker-compose.xr.yml \
-  --output-file ~/sandbox/topologies/segment-routing/docker-compose.yml \
+  --input-file ~/XRd-Sandbox/topologies/segment-routing/docker-compose.xr.yml \
+  --output-file ~/XRd-Sandbox/topologies/segment-routing/docker-compose.yml \
   --image xrd-control-plane:latest-24.4
 
-sed -i.bak 's/linux:xr-120/linux:eth0/g' ~/sandbox/topologies/segment-routing/docker-compose.yml
+sed -i.bak 's/linux:xr-120/linux:eth0/g' ~/XRd-Sandbox/topologies/segment-routing/docker-compose.yml
 
-docker compose --file ~/sandbox/topologies/segment-routing/docker-compose.yml up --detach
+docker compose --file ~/XRd-Sandbox/topologies/segment-routing/docker-compose.yml up --detach
